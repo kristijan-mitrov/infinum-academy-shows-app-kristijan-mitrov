efafd6c067e31e189b8fe85fff641e7fa6ac503a
DESKTOP-1OPK6VK\mitro <kristijan.mitrov@student.um.si>
2022-08-10 11:56:33 +0200

    Splash Screen

--- app/src/main/java/shows/kristijanmitrov/infinumacademyshows/MainActivity.kt
+++ app/src/main/java/shows/kristijanmitrov/infinumacademyshows/MainActivity.kt
@@ -1,15 +1,24 @@
 package shows.kristijanmitrov.infinumacademyshows
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import shows.kristijanmitrov.infinumacademyshows.databinding.ActivityMainBinding
+import shows.kristijanmitrov.viewModel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen().apply {
+            setKeepOnScreenCondition{
+                viewModel.isLoading.value
+            }
+        }
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
